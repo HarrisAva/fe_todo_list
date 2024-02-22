7a391ccc52e1fafb435feb9d82dabfdba1febcdd
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2024-02-22 10:43:41 -0600

    complete authentication and login/logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoListComponent, FormsModule],
+  imports: [TodoListComponent, FormsModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
